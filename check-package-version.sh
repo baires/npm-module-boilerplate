6af4f224b292563d054a1014fa7efc832f88241a
Alexis Sgarbossa <alexis@sgarbossa.com.ar>
2017-03-30 15:26:20 -0300

    Hello

---
+++ check-package-version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NPM_PACKAGE_VERSION=$(npm info @tactivos/mural-shared version)
+
+MY_PACKAGE_VERSION=$(node -pe 'require("./package.json").version')
+
+# if [ "$MY_PACKAGE_VERSION" == "$NPM_PACKAGE_VERSION" ]; then
+#   printf "Same version [%s]\n" "$MY_PACKAGE_VERSION"
+#   exit 1
+# fi
+
+if [ "$MY_PACKAGE_VERSION" \> "$NPM_PACKAGE_VERSION" ]; then
+  printf "Old version [%s]\n" "$MY_PACKAGE_VERSION"
+  exit 1
+
+else [ "$MY_PACKAGE_VERSION" \< "$NPM_PACKAGE_VERSION" ]
+  printf "New version [%s] === [%s]\n" "$MY_PACKAGE_VERSION" "$NPM_PACKAGE_VERSION"
+  exit 0
+fi
