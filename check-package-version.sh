851280b78d176d08c36d40eeed939c08b6188a1c
Alexis Sgarbossa <alexis@sgarbossa.com.ar>
2017-03-31 10:15:35 -0300

    Bump

--- check-package-version.sh
+++ check-package-version.sh
@@ -4,7 +4,6 @@ REPO=baires/npm-module-boilerplate
 NPM_PACKAGE_VERSION=$(npm info @baires/number-formatter)
 MY_PACKAGE_VERSION=$(node -pe 'require("./package.json").version')
 
-
  if [ "$(printf "$NPM_PACKAGE_VERSION\n$MY_PACKAGE_VERSION" | sort -V | head -n1)" == "$MY_PACKAGE_VERSION" ] && [ "$MY_PACKAGE_VERSION" != "$NPM_PACKAGE_VERSION" ]; then
    printf "Old version [%s]\n" "$MY_PACKAGE_VERSION"
    curl -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" \
